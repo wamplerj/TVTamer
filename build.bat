6d1c087e25da097ae69a20b77433e72801c5430c
James Wampler <james@jameswampler.com>
2015-08-14 10:10:27 -0700

    Replaced build with FAKE

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
-
 @ECHO OFF
-echo Building tvtamer solution
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild" build.proj /t:build /p:Configuration=Release
+
+cls
+"packages\FAKE.4.1.1\tools\Fake.exe" build.fsx
+REM pause
+
+
+REM "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild" build.proj /t:build /p:Configuration=Release
 
